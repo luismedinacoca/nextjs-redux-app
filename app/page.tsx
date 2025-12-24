f071390f93dcde26fda2404d2d467a71907bce92
Luis Medina Coca <luismedinacoca@gmail.com>
2025-12-24 18:00:40 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="p-8">
+      <h1 className="text-center scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Complete Redux Toolkit in Next JS with Typescript
+      </h1>
+      <ul className="text-xl my-6 ml-6 list-decimal [&>li]:mt-2">
+        <li>Installation and Setup</li>
+        <li>Implement Counter with Redux </li>
+        <li>Implement Cart Functionality with Redux</li>
+        <li>Implement Multi step form with Redux</li>
+      </ul>
+    </div>
+  );
+}
